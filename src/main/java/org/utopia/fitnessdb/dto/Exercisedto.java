df838ee065bb254bc8ea4e49a26c14dc44562693
CedTheLearner <cedrick21.den22@gmail.com>
2025-10-21 14:15:49 -0400

    Update Exercisedto.java

--- src/main/java/org/utopia/fitnessdb/dto/Exercisedto.java
+++ src/main/java/org/utopia/fitnessdb/dto/Exercisedto.java
@@ -1,1 +1,23 @@
 
+package org.utopia.fitnessdb.dto;
+
+import java.time.Instant;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Value;
+
+@Builder
+@Data
+@Value
+public class Exercisedto {
+
+  Integer exerciseId;
+
+  String exerciseName;
+
+  String note;
+
+  Instant duration;
+
+  Integer reps;
+}
