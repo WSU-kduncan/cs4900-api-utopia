f3b3724c8c0a4df98ddc4e777836c303bcb5c36e
CedTheLearner <cedrick21.den22@gmail.com>
2025-10-21 15:10:39 -0400

    Update ExerciseDtoMapper.java

--- src/main/java/org/utopia/fitnessdb/mapper/ExerciseDtoMapper.java
+++ src/main/java/org/utopia/fitnessdb/mapper/ExerciseDtoMapper.java
@@ -8,24 +8,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import org.utopia.fitnessdb.controller.RoutineController;
 import org.utopia.fitnessdb.controller.ExerciseController;
-
-import org.utopia.fitnessdb.dto.RoutineDto;
 import org.utopia.fitnessdb.dto.ExerciseDto;
-
-import org.utopia.fitnessdb.mapper.RoutineDtoMapper;
 import org.utopia.fitnessdb.mapper.ExerciseDtoMapper;
-
-import org.utopia.fitnessdb.repository.RoutineRepository;
 import org.utopia.fitnessdb.repository.ExerciseRepository;
-
 import org.utopia.fitnessdb.service.ExerciseService;
-import org.utopia.fitnessdb.service.RoutineService;
-
 import org.utopia.fitnessdb.model.Exercise;
-import org.utopia.fitnessdb.model.Routine;
 
 import java.util.List;
 
