bb4b16bb02905c534ab6e6eca4ab10497b7f9b06
CedTheLearner <cedrick21.den22@gmail.com>
2025-10-21 15:03:34 -0400

    Update and rename Exercisecontroller.java to ExerciseController.java

--- src/main/java/org/utopia/fitnessdb/controller/ExerciseController.java
+++ src/main/java/org/utopia/fitnessdb/controller/ExerciseController.java
@@ -8,24 +8,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import org.utopia.fitnessdb.controller.RoutineController;
 import org.utopia.fitnessdb.controller.ExerciseController;
-
-import org.utopia.fitnessdb.dto.RoutineDto;
 import org.utopia.fitnessdb.dto.ExerciseDto;
-
-import org.utopia.fitnessdb.mapper.RoutineDtoMapper;
 import org.utopia.fitnessdb.mapper.ExerciseDtoMapper;
-
-import org.utopia.fitnessdb.repository.RoutineRepository;
 import org.utopia.fitnessdb.repository.ExerciseRepository;
-
 import org.utopia.fitnessdb.service.ExerciseService;
-import org.utopia.fitnessdb.service.RoutineService;
-
 import org.utopia.fitnessdb.model.Exercise;
-import org.utopia.fitnessdb.model.Routine;
 
 import java.util.List;
 
@@ -35,21 +23,21 @@ import java.util.List;
     path = "exercise",
     produces = MediaType.APPLICATION_JSON_VALUE,
     consumes = MediaType.APPLICATION_JSON_VALUE)
-public class Exercisecontroller {
+public class ExerciseController {
 
-  private final Exercisedtomapper exercisedtomapper;
+  private final ExerciseDtoMapper exerciseDtoMapper;
 
-  private final Exerciseservice exerciseservice;
+  private final ExerciseService exerciseService;
 
   @GetMapping
-  ResponseEntity<List<Exercisedto>> getAllExercise() {
+  ResponseEntity<List<ExerciseDto>> getAllExercise() {
     return new ResponseEntity<>(
-        ExercisedtoMapper.toDtoList(exerciseservice.getAllExercise()), HttpStatus.OK);
+        ExerciseDtoMapper.toDtoList(exerciseService.getAllExercise()), HttpStatus.OK);
   }
 
   @GetMapping(path = "{id}")
-  ResponseEntity<Exercisedto> getExerciseById(@PathVariable Integer id) {
+  ResponseEntity<ExerciseDto> getExerciseById(@PathVariable Integer id) {
     return new ResponseEntity<>(
-        Exercisedtomapper.toDto(exerciseservice.getExerciseById(id)), HttpStatus.OK);
+        ExerciseDtoMapper.toDto(exerciseService.getExerciseById(id)), HttpStatus.OK);
   }
 }
