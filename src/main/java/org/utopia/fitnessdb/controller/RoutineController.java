59ac82324e95a7aa180429a5cdbd671a65adc667
CedTheLearner <cedrick21.den22@gmail.com>
2025-10-21 15:08:06 -0400

    Update and rename Routinecontroller.java to RoutineController.java

--- src/main/java/org/utopia/fitnessdb/controller/RoutineController.java
+++ src/main/java/org/utopia/fitnessdb/controller/RoutineController.java
@@ -9,23 +9,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import org.utopia.fitnessdb.controller.RoutineController;
-import org.utopia.fitnessdb.controller.ExerciseController;
-
 import org.utopia.fitnessdb.dto.RoutineDto;
-import org.utopia.fitnessdb.dto.ExerciseDto;
-
 import org.utopia.fitnessdb.mapper.RoutineDtoMapper;
-import org.utopia.fitnessdb.mapper.ExerciseDtoMapper;
-
 import org.utopia.fitnessdb.repository.RoutineRepository;
-import org.utopia.fitnessdb.repository.ExerciseRepository;
-
-import org.utopia.fitnessdb.service.ExerciseService;
 import org.utopia.fitnessdb.service.RoutineService;
-
-import org.utopia.fitnessdb.model.Exercise;
 import org.utopia.fitnessdb.model.Routine;
 
 import java.util.List;
@@ -36,21 +24,21 @@ import java.util.List;
     path = "routine",
     produces = MediaType.APPLICATION_JSON_VALUE,
     consumes = MediaType.APPLICATION_JSON_VALUE)
-public class Routinecontroller {
+public class RoutineController {
 
-  private final Routinedtomapper routinedtomapper;
+  private final RoutineDtoMapper routineDtoMapper;
 
-  private final Routineservice routineservice;
+  private final RoutineService routineService;
 
   @GetMapping
-  ResponseEntity<List<Routinedto>> getAllRoutine() {
+  ResponseEntity<List<RoutineDto>> getAllRoutine() {
     return new ResponseEntity<>(
-        routinedtoMapper.toDtoList(routineservice.getAllRoutine()), HttpStatus.OK);
+        routineDtoMapper.toDtoList(routineService.getAllRoutine()), HttpStatus.OK);
   }
 
   @GetMapping(path = "{id}")
-  ResponseEntity<Routinedto> getRoutineById(@PathVariable Integer id) {
+  ResponseEntity<RoutineDto> getRoutineById(@PathVariable Integer id) {
     return new ResponseEntity<>(
-        Routinedtomapper.toDto(routineservice.getRoutineById(id)), HttpStatus.OK);
+        routineDtoMapper.toDto(routineService.getRoutineById(id)), HttpStatus.OK);
   }
 }
