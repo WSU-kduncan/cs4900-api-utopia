c14c06950804191be064a73dbf4e6d80e70cdde4
CedTheLearner <cedrick21.den22@gmail.com>
2025-10-21 15:18:14 -0400

    Create H2TcpConfig.java

---
+++ src/main/java/org/utopia/fitnessdb/config/H2TcpConfig.java
@@ -0,0 +1,19 @@
+package org.utopia.fitnessdb.config;
+import java.sql.SQLException;
+import lombok.extern.log4j.Log4j2;
+import org.h2.tools.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Log4j2
+@Profile("h2")
+public class H2TcpConfig {
+
+  @Bean(initMethod = "start", destroyMethod = "stop")
+  public Server inMemoryH2DatabaseServer() throws SQLException {
+    log.info("*** H2TcpConfig: H2 tcp access activated on port 9092 ***");
+    return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
+  }
+}
