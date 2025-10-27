507b368160bc89c98fd2f686ec78b2e45cf8c1ef
Kiran BIswa <kiran.biswa682@gmail.com>
2025-10-27 13:45:46 -0400

    testing

--- src/main/java/org/utopia/fitnessdb/model/Client.java
+++ src/main/java/org/utopia/fitnessdb/model/Client.java
@@ -1,9 +1,17 @@
 package org.utopia.fitnessdb.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
 import lombok.Getter;
 import lombok.Setter;
-import jakarta.persistence.*;
-//import java.util.List;
 
 @Entity
 @Table(name = "Client")
@@ -13,13 +21,13 @@ public class Client {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "client_id", nullable = false)
-    private Integer clientId;
+    private Integer id;
 
     @Column(name = "client_name", nullable = false, length = 50)
-    private String clientName;
+    private String name;
 
     @Column(name = "client_email", nullable = false, unique = true, length = 50)
-    private String clientEmail;
+    private String email;
 
     @Column(name = "password_hash", nullable = false, length = 255)
     private String passwordHash;
