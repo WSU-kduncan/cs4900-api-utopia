40246b508881c154f17731d0ab1b63d80addf513
Kiran BIswa <kiran.biswa682@gmail.com>
2025-10-31 10:08:34 -0400

    added POST and PUT

--- src/main/java/org/utopia/fitnessdb/controller/SessionController.java
+++ src/main/java/org/utopia/fitnessdb/controller/SessionController.java
@@ -7,12 +7,18 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.utopia.fitnessdb.dto.SessionDto;
 import org.utopia.fitnessdb.mapper.SessionDtoMapper;
 import org.utopia.fitnessdb.service.SessionService;
 
+import jakarta.persistence.criteria.Path;
+
+import java.util.Date;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -35,4 +41,41 @@ public class SessionController {
         return new ResponseEntity<>(
                 sessionDtoMapper.toDto(sessionService.getSessionById(id)), HttpStatus.OK);
     }
+/* 
+    // get session by date
+    @GetMapping(path = "{date}")
+    ResponseEntity<SessionDto> getSessionByDate(@PathVariable Date date) {
+        return new ResponseEntity<>(
+            SessionDtoMapper.toDto(sessionService.getSessionByDate(date)), HttpStatus.OK);
+    } */
+
+    // get session by trainer
+    @GetMapping(path = "trainer/{trainer}")
+    ResponseEntity<SessionDto> getSessionByTrainer(@PathVariable Integer trainer) {
+        return new ResponseEntity<>(
+            sessionDtoMapper.toDto(sessionService.getSessionByTrainer(trainer)), HttpStatus.OK);
+    }
+
+    // get session by client
+    @GetMapping(path = "client/{client}")
+    ResponseEntity<SessionDto> getSessionByClient(@PathVariable Integer client) {
+        return new ResponseEntity<>(
+            sessionDtoMapper.toDto(sessionService.getSessionByClient(client)), HttpStatus.OK);
+    }
+
+ 
+    // POST
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    ResponseEntity<SessionDto> createSession(@RequestBody SessionDto sessionDto) {
+        SessionDto created = sessionDtoMapper.toDto(sessionService.createSession(sessionDtoMapper.toEntity(sessionDto)));
+        return new ResponseEntity<>(created, HttpStatus.CREATED);
+    }
+
+    // PUT 
+    @PutMapping(path = "{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    ResponseEntity<SessionDto> updateSession(@PathVariable Integer id, @RequestBody SessionDto sessionDto) {
+        SessionDto updated = sessionDtoMapper.toDto(sessionService.updateSession(id, sessionDtoMapper.toEntity(sessionDto)));
+        return new ResponseEntity<>(updated, HttpStatus.OK);
+    }
+
 }
--- src/main/java/org/utopia/fitnessdb/service/SessionService.java
+++ src/main/java/org/utopia/fitnessdb/service/SessionService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.utopia.fitnessdb.model.Session;
 import org.utopia.fitnessdb.repository.SessionRepository;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,4 +28,50 @@ public class SessionService {
         }
         return session.get();
     }
+
+    public Session getSessionByTrainer(Integer trainer) throws EntityNotFoundException {
+        Optional<Session> session = sessionRepository.findByTrainerId(trainer);
+        if (session.isEmpty()) {
+            throw new EntityNotFoundException("Session with trainer ID " + trainer + " not found");
+        }
+        return session.get();
+    }
+
+    public Session getSessionByClient(Integer client) throws EntityNotFoundException {
+        Optional<Session> session = sessionRepository.findByClientId(client);
+        if (session.isEmpty()) {
+            throw new EntityNotFoundException("Session with client ID " + client + " not found");
+        }
+        return session.get();
+    }
+
+    public Session createSession(Session entity) {
+        return sessionRepository.save(entity);
+
+    }
+
+    public Session updateSession(Integer id, Session update) throws EntityNotFoundException {
+        Session existingSession = sessionRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Session with ID: " + id + " not found"));
+    
+        // Only update fields that are not null
+        if (update.getDate() != null) existingSession.setDate(update.getDate());
+        if (update.getDuration() != null) existingSession.setDuration(update.getDuration());
+        if (update.getNote() != null) existingSession.setNote(update.getNote());
+        if (update.getTrainer() != null) existingSession.setTrainer(update.getTrainer());
+        if (update.getClient() != null) existingSession.setClient(update.getClient());
+        if (update.getRoutine() != null) existingSession.setRoutine(update.getRoutine());
+    
+        return sessionRepository.save(existingSession);
+    }
+    
+
+
+
+    //public Session getSessionByDate(Session date) {
+      //  return date; 
+    //}
+
+    
+
 }
