7262f998f041268fa81333954b87d119ac4318ae
Kiran BIswa <kiran.biswa682@gmail.com>
2025-10-31 10:08:34 -0400

    added POST and PUT

--- src/main/java/org/utopia/fitnessdb/service/SessionService.java
+++ src/main/java/org/utopia/fitnessdb/service/SessionService.java
@@ -26,4 +26,40 @@ public class SessionService {
         }
         return session.get();
     }
+
+    public Session getSessionByTrainer(Integer trainer) throws EntityNotFoundException {
+        Optional<Session> session = sessionRepository.findByTrainerId(trainer);
+        if (session.isEmpty()) {
+            throw new EntityNotFoundException("Session with trainer ID " + trainer + " not found");
+        }
+        return session.get();
+    }
+
+    public Session getSessionByClient(Integer client) throws EntityNotFoundException {
+        Optional<Session> session = sessionRepository.findByClientId(client);
+        if (session.isEmpty()) {
+            throw new EntityNotFoundException("Session with client ID " + client + " not found");
+        }
+        return session.get();
+    }
+
+    public Session createSession(Session entity) {
+        return sessionRepository.save(entity);
+
+    }
+
+    public Session updateSession(Integer id, Session update) throws EntityNotFoundException {
+        Session existingSession = sessionRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Session with ID: " + id + " not found"));
+
+        // Only update fields that are not null
+        if (update.getDate() != null) existingSession.setDate(update.getDate());
+        if (update.getDuration() != null) existingSession.setDuration(update.getDuration());
+        if (update.getNote() != null) existingSession.setNote(update.getNote());
+        if (update.getTrainer() != null) existingSession.setTrainer(update.getTrainer());
+        if (update.getClient() != null) existingSession.setClient(update.getClient());
+        if (update.getRoutine() != null) existingSession.setRoutine(update.getRoutine());
+
+        return sessionRepository.save(existingSession);
+    }
 }
