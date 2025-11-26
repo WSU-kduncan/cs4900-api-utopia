51b7e1dc62e6f20a14aab6e63e35bb0ea34b92ee
Kiran <kiran.biswa682@gmail.com>
2025-11-25 20:15:30 -0500

    fixed path to accept 4200

--- src/main/java/org/utopia/fitnessdb/controller/SessionController.java
+++ src/main/java/org/utopia/fitnessdb/controller/SessionController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,7 +25,8 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping(path = "session", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(path = "session")
+@CrossOrigin(origins = "http://localhost:4200")
 public class SessionController {
     private final SessionDtoMapper sessionDtoMapper;
     private final SessionService sessionService;
